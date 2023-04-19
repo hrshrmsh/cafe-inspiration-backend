21b16456ba3967f36546321d4eef8fb0791bf12e
harsh <harshavardhan_ramesh@outlook.com>
2023-04-18 22:00:27 -0500

    first commit

---
+++ main/src/test/java/moe/cafe/inspiration/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.cafe.inspiration.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
